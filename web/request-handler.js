86e504e984299bc19179c7787fcbb0de90e0d9de
15chrjef <15chrjef@gmail.com>
2016-10-05 23:34:54 -0700

    hello

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,18 +3,31 @@ var archive = require('../helpers/archive-helpers');
 var requestHelpers = require('./http-helpers');
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  var url = archive.paths.siteAssets + './index.html';
-  fs.readFile(url, function(err, file) {
-    if (err) {
-      console.error(err);
-    } else {
-      res.writeHead(200, requestHelpers.headers);
-      res.write(file);
-      res.end();
-    }
-  });
+  console.log('requrl***********',req.url)
+  var url = archive.paths.siteAssets + '/index.html';
+  //where we store our lookup value for the archives
+  var input;
+  console.log(url);
+
+  if (req.method === 'GET') {
+    fs.readFile(url, 'utf-8', function(err, chunk) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(chunk)
+        res.writeHead(200, {
+          'Content-Type': 'text/plain'
+        });
+        res.write(chunk);
+        res.end();
+        return;
+      }
+    });
+  }
+  
 };
