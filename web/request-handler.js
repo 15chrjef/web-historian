61aee04b814615eb4d2db7ab6937833b9f6e7a12
15chrjef <15chrjef@gmail.com>
2016-10-05 21:19:33 -0700

    accessed index.html for read

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var requestHelpers = require('./http-helpers');
+var http = require('http');
+var url = require('url');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var url = archive.paths.siteAssets + './index.html';
+  fs.readFile(url, function(err, file) {
+    if (err) {
+      console.error(err);
+    } else {
+      res.writeHead(200, requestHelpers.headers);
+      res.write(file);
+      res.end();
+    }
+  });
 };
